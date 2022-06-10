b9f9bc5fe04a7961c067e0ad806d7007f515de8c
Jiwoong Jung <kanziwoong@gmail.com>
2022-06-11 01:38:30 +0900

    Update ESLint rule for React (#2)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,17 @@
 module.exports = {
   extends: [
     'kanziw',
+    'plugin:react/recommended',
   ],
   parserOptions: {
-    tsconfigRootDir: __dirname,
+    ecmaFeatures: {
+      jsx: true,
+    },
     project: './tsconfig.eslint.json',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
   },
 }
